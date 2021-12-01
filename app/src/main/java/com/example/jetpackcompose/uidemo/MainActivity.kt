979c67e64353a71805b545cdb1cef75afdd91652
ogayu-0921 <ogayu0921@gmail.com>
2021-12-01 09:37:33 +0900

    ファイル追加

--- app/src/main/java/com/example/jetpackcompose/uidemo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/uidemo/MainActivity.kt
@@ -9,30 +9,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose.uidemo.ui.theme.UiDemoTheme
+import com.example.jetpackcompose.uidemo.ui.view.TopView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UiDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            TopView()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    UiDemoTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/jetpackcompose/uidemo/ui/view/TopView.kt
@@ -0,0 +1,8 @@
+package com.example.jetpackcompose.uidemo.ui.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TopView(){
+
+}
